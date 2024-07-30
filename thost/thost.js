2ce623efdca16582810a723c3740ccf2eea3f66f
= <muthuannamalai@example.com>
2024-07-30 22:20:49 +0100

    now

--- thost/thost.js
+++ thost/thost.js
@@ -1,1 +1,18 @@
-export const thost=t=>{const e=`<div id='thethost' class="thostjs">\n        <div class="data">\n            <p>${t}</p>\n        </div>\n    </div>`;setTimeout((()=>{document.getElementById("thethost").classList.add("thosefadeout")}),5e3),document.querySelector("body").innerHTML+=e};
+export const thost = (thostData) => {
+    const write = {
+        message: thostData
+    }
+
+    const thostComponent = `<div id='thethost' class="thostjs">
+        <div class="data">
+            <p>${write.message}</p>
+        </div>
+    </div>`
+
+    setTimeout(() => {
+        document.getElementById('thethost').classList.add('thosefadeout');
+    }, 5000)
+
+    document.querySelector('body').innerHTML += thostComponent
+}
+
